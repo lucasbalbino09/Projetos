e6e3267760c1b15a016b18c3a40e7ab90606c696
lucasbalbino097 <soprogresso2409@gmail.com>
2022-07-14 19:32:38 -0300

    Atualizando Atividades

---
+++ Reforco/Course/Course/Triangulo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Course
+{
+    internal class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+
+
+        public double Area()
+        {            
+            double P = (A + B + C) / 2.0;
+            return Math.Sqrt(P * (P - A) * (P - B) * (P - C));
+        }
+
+    }
+}
---
+++ Reforco/Estoque/Estoque/Produto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+namespace Estoque
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+
+
+        public override string ToString()
+        {
+            return Nome
+                + " , "
+                + "R$ "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture) 
+                + " , "
+                + Quantidade
+                + " Unidades, " 
+                + "Total: R$ " 
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
