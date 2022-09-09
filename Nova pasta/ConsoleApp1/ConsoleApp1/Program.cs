551e615804437425f6ec06203eac026708b3802a
lucas.balbino <lucas.balbino@financeiro24horas.com>
2022-09-09 16:02:39 -0300

    enviando arquivo para atualizar o git

---
+++ Nova pasta/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double? x = null;
+            double? y = 10.00;
+
+
+            Console.WriteLine(x.GetValueOrDefault());
+
+        }
+    }
+}
