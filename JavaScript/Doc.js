7fc29d9c5c5d8499621644523d1e5128db46104c
lucasbalbino09 <balbino2409@gmail.com>
2022-09-10 19:01:51 -0300

    Treinando a linguagem Javascript.

---
+++ JavaScript/Doc.js
@@ -0,0 +1,9 @@
+// let notaDoPrimeiroB = parseInt(prompt("Sua primeira nota?"));
+// let notaDoSegundoB = parseInt(prompt("Sua Segunda nota?"));
+// let notaDoTerceiroB = parseInt(prompt("Sua Terceira nota?"));
+// let notaDoQuartoB = parseInt(prompt("Sua Quarta e ultima nota?"));
+let notaFinal = (notaDoPrimeiroB + notaDoSegundoB + notaDoTerceiroB + notaDoQuartoB) / 4; 
+
+let notafixada = notaFinal.toFixed(1);
+
+alert(notafixada);
