29f0e3093c69e67a5bd449ba32513b6a95334a95
lucas.balbino <lucas.balbino@financeiro24horas.com>
2022-09-12 10:42:11 -0300

    Organizando o repositorio

---
+++ JavaScript/ProjetoJS(Pt1)/exercicio.js
@@ -0,0 +1,3 @@
+confirm("Está gostando de javascript ? ");
+let nome = prompt("Qual Seu nome ? ");
+alert(`Olá ${nome} `);
---
+++ JavaScript/ProjetoJS(Pt2)/SegExercicio.js
@@ -0,0 +1,3 @@
+let nome = prompt("Olá, qual seu nome ? "); // Não é mais utilizado o Var, para declarar variaveis. 
+alert(`Bem vindo ${nome}`); // Reutilizei a variavel com o interpolação. 
+confirm("Sinta-se em casa. "); // esse é interesante.
