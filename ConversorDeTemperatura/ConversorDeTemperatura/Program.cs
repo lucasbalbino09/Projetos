21781b119cf3aed8b130c90812d3779dad502dc8
lucas.balbino <98754988+lucasbalbino09@users.noreply.github.com>
2023-01-06 11:55:11 -0300

    Conversor_de_Temperatura
    Conversor de Temperatura em C#

---
+++ ConversorDeTemperatura/ConversorDeTemperatura/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConversorDeTemperatura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Conversor!");
+
+
+            Console.WriteLine("Valor em Celsius!");
+            double Cels = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Valor em Fahrenheit!");
+            double Fahr = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            var ValorC = Temperatura.ConversorCelsius(Cels);
+
+            Console.WriteLine($"Valor em Celsius: {ValorC.ToString("F2", CultureInfo.InvariantCulture)} ");
+
+            var ValorF = Temperatura.ConversorFahrenheit(Fahr);
+
+            Console.WriteLine($"Valor em Fahrenheit {ValorF.ToString("F2", CultureInfo.InvariantCulture)}");
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ConversorDeTemperatura/ConversorDeTemperatura/Temperatura.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorDeTemperatura
+{
+    class Temperatura
+    {
+        public static double Celsius;
+        public static double Fahrenheit;
+
+
+        public static double ConversorCelsius(double TempF)
+        {
+            return Celsius = (TempF - 32) / 1.8;
+        }
+
+
+        public static double ConversorFahrenheit(double TempC)
+        {
+            return Fahrenheit = (TempC * 1.8) + 32;
+        }
+
+    }
+}
