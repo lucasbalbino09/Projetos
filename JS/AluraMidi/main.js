722466921c19ba3f3bb15ef691348b8b3637c84f
lucas.balbino <98754988+lucasbalbino09@users.noreply.github.com>
2023-01-24 17:18:29 -0300

    Repositorio atualizado

--- JS/AluraMidi/main.js
+++ JS/AluraMidi/main.js
@@ -1,2 +1,26 @@
-document.querySelector('.tecla_pom'); //Seleciona um elemento no meu html
-document.querySelector('#som_tecla_pom').play(); //Seleciona um elemento no meu html
+function TocaSom(idElementoAudio) {
+
+    document.querySelector(idElementoAudio).play(); //Seleciona um elemento no meu html
+}
+const ListaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let i = 0; i < ListaDeTeclas.length; i++) {
+
+    const tecla = ListaDeTeclas[i];
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`;
+    console.log(instrumento);
+
+    tecla.onclick = function() {
+        TocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function() {
+        tecla.classList.add('ativa');
+    }
+
+    tecla.onkeyup = function() {
+        tecla.classList.remove('ativa');
+    }
+}
