c8310fc572a5b3f67d7bbe4b3ffba5e0b0fe6ef8
lucas.balbino <98754988+lucasbalbino09@users.noreply.github.com>
2023-01-26 12:12:14 -0300

    Ultima Atualização

--- JS/AluraMidi/main.js
+++ JS/AluraMidi/main.js
@@ -1,7 +1,18 @@
-function TocaSom(idElementoAudio) {
+function TocaSom(SeletorAudio) {
 
-    document.querySelector(idElementoAudio).play(); //Seleciona um elemento no meu html
+    const elemento = document.querySelector(SeletorAudio).play(); //Seleciona um elemento no meu html
+
+    if (elemento && elemento != null && elemento === 'audio') {
+
+        elemento.play();
+
+    } else {
+        console.log('Elemento não encontrado ou Seletor invalido!');
+    }
+
+
 }
+
 const ListaDeTeclas = document.querySelectorAll('.tecla');
 
 for (let i = 0; i < ListaDeTeclas.length; i++) {
@@ -10,14 +21,19 @@ for (let i = 0; i < ListaDeTeclas.length; i++) {
     const instrumento = tecla.classList[1];
 
     const idAudio = `#som_${instrumento}`;
-    console.log(instrumento);
 
     tecla.onclick = function() {
+
         TocaSom(idAudio);
     }
 
-    tecla.onkeydown = function() {
-        tecla.classList.add('ativa');
+    tecla.onkeydown = function(evento) {
+
+        console.log(evento.code);
+        if (evento.code === 'Enter' || evento.code === 'Space') {
+
+            tecla.classList.add('ativa');
+        }
     }
 
     tecla.onkeyup = function() {
